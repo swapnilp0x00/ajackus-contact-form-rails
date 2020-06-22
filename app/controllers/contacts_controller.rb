f3ce6c7a78ecda0a160d613f3cb60511198f9f2c
swapnil <ddevil691@gmail.com>
2020-06-23 01:02:13 +0530

    Removed unwanted code

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,29 +1,13 @@
 class ContactsController < ApplicationController
-  # before_action :set_contact, only: [:show, :edit, :update, :destroy]
   before_action :set_locale, only: [:new]
-  # GET /contacts
-  # GET /contacts.json
-  # def index
-  #   @contacts = Contact.all
-  # end
-
-  # GET /contacts/1
-  # GET /contacts/1.json
-  # def show
-  # end
 
   # GET /contacts/new
   def new
-    @languages = I18n.available_locales
+    @supported_languages = I18n.available_locales
     @contact = Contact.new
   end
 
-  # GET /contacts/1/edit
-  # def edit
-  # end
-
   # POST /contacts
-  # POST /contacts.json
   def create
     @contact = Contact.new(contact_params)
 
@@ -37,30 +21,6 @@ class ContactsController < ApplicationController
       end
     end
   end
-
-  # PATCH/PUT /contacts/1
-  # PATCH/PUT /contacts/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @contact.update(contact_params)
-  #       format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @contact }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @contact.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # DELETE /contacts/1
-  # DELETE /contacts/1.json
-  # def destroy
-  #   @contact.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'contacts#new'
   get 'contacts', to: 'contacts#new'
   post 'contacts', to: 'contacts#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
