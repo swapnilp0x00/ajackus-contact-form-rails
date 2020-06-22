4d728f0cbaa30707c22434b14dcacdb4c5f5630c
swapnil <ddevil691@gmail.com>
2020-06-22 23:25:13 +0530

    Removed css

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -28,8 +28,8 @@ class ContactsController < ApplicationController
 
     respond_to do |format|
       if @contact.save
-        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
-        format.json { render :show, status: :created, location: @contact }
+        format.html { redirect_to contacts_url, notice: 'Contact was successfully created.' }
+        format.json { render :new, status: :created, location: @contact }
       else
         format.html { render :new }
         format.json { render json: @contact.errors, status: :unprocessable_entity }
