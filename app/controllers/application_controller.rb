5adff8905097995b5bd5c7c7b06229ebe362a961
swapnil <ddevil691@gmail.com>
2020-06-23 02:03:38 +0530

    Extracted methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    before_action :set_supported_languages
+    before_action :set_locale
+
+    def set_supported_languages
+        @supported_languages = I18n.available_locales
+    end
+
+    def set_locale
+        query_locale = params[:locale]
+        valid_locale = I18n.available_locales.map(&:to_s).include?(query_locale)
+        if valid_locale
+          I18n.locale = query_locale
+        else
+          I18n.locale = I18n.default_locale
+        end
+    end
 end
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,13 @@
 class ContactsController < ApplicationController
-  before_action :set_locale, only: [:new]
 
   # GET /contacts/new
   def new
-    @supported_languages = I18n.available_locales
     @contact = Contact.new
   end
 
   # POST /contacts
   def create
     @contact = Contact.new(contact_params)
-
     respond_to do |format|
       if @contact.save
         format.html { redirect_to contacts_url, notice: 'Contact was successfully created.' }
@@ -28,15 +25,6 @@ class ContactsController < ApplicationController
     #   @contact = Contact.find(params[:id])
     # end
 
-    def set_locale
-      query_locale = params[:locale]
-      valid_locale = I18n.available_locales.map(&:to_s).include?(query_locale)
-      if valid_locale
-        I18n.locale = query_locale
-      else
-        I18n.locale = I18n.default_locale
-      end
-    end
     # Only allow a list of trusted parameters through.
     def contact_params
       params.require(:contact).permit(:first_name, :last_name, :email, :phone, :message)
