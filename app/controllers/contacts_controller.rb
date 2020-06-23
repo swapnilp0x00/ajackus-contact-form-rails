88cd478371b066a344fa98a5e293491b8f696c09
swapnil <ddevil691@gmail.com>
2020-06-24 01:47:24 +0530

    Adding query parameter to post URL

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -20,11 +20,6 @@ class ContactsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    # def set_contact
-    #   @contact = Contact.find(params[:id])
-    # end
-
     # Only allow a list of trusted parameters through.
     def contact_params
       params.require(:contact).permit(:first_name, :last_name, :email, :phone, :message)
