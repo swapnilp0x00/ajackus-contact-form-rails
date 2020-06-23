ed4b64e7f5212c96f99acd9dbedf274e2331516e
swapnil <ddevil691@gmail.com>
2020-06-24 02:18:39 +0530

    Change selector to links and styled

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module ContactForm2
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    I18n.available_locales = [:de, :en]
+    I18n.available_locales = [:en, :de]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml')]
     config.i18n.default_locale = :en
   end
