eac69c4b9d14f164de9e112d1a98978b0694b8a2
swapnil <ddevil691@gmail.com>
2020-06-24 01:51:08 +0530

    Added jquery using webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,13 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 
-
+require('jquery');
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+console.log($);
